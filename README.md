de32671d2c45fe0cb4b4ab3c2ded37631a011ff4
zzy <ilmare@buaa.edu.cn>
2018-07-01 10:44:43 +0800

    完成了机构注册页面和会议展示页面，会议展示页面还没有测试，还有一点小小的bug。更新了readme

--- README.md
+++ README.md
@@ -3,4 +3,14 @@ BuaaSummer Front-End part
 
 项目结构：
 -conference:会议相关页面
--libs:开发需要的外部文件如bootstrap,jquery
+|----conference-main.html:会议展示页面
+|----leftnav.html:会议展示页面左侧导航栏
+|----Navigation.html:所有页面顶部导航栏
+|----paper.docx:用于测试下载的论文模板
+|----other.zip:用于测试下载的其他参考资料
+-institution:机构相关页面
+|----institution-register.html:机构注册页面
+-libs:开发需要的外部文件
+
+项目运行说明：因为conference目录下页面使用了jquery.load()方法，所以需要在Front_End目录下启动服务器，
+再输入对应的url才能正常访问conference页面，直接打开页面只能看到一部分页面。
--- libs/js/solo.js
+++ libs/js/solo.js
@@ -1,4 +1,100 @@
+
+
+$(window).scroll(function(){
+	let bar = document.documentElement.scrollTop;
+	let contents = document.getElementsByClassName('content');	let now=0;
+	let arr=[];
+	for(let x=0;x<contents.length;x++)
+	{
+		arr[x]=contents[x].offsetTop;
+		if(arr[x]-150<bar)
+		{
+			now = x;
+		}
+		else{
+			break;
+		}
+	}
+	for(let x=0;x<contents.length;x++)
+	{
+		$('#'+($(contents[x]).attr('name'))+'_link').attr('class','aa list-group-item scroll');
+	}
+	$('#'+($(contents[now]).attr('name'))+'_link').attr('class','aa list-group-item scroll now');
+})
+
 function reference(){
-	$('#papertemplate_btn').toggle();
-	$('#otherref_btn').toggle();
+	$('#papertemplate').toggle();
+	$('#otherref').toggle();
 }
+
+function toRegistration(){
+	alert('to Registration!');
+}
+
+function toReserve(){
+	alert('to Reserve!');
+}
+
+function now_general(){
+	$('.aa').each(function(){
+		$(this).attr('class','aa list-group-item scroll');
+	});
+	$('#general_link').attr('class','now aa list-group-item scroll');
+}
+
+function now_essay(){
+	$('.aa').each(function(){
+		$(this).attr('class','aa list-group-item scroll');
+	});
+	$('#essay_link').attr('class','now aa list-group-item scroll');
+}
+
+function now_important(){
+	$('.aa').each(function(){
+		$(this).attr('class','aa list-group-item scroll');
+	});
+	$('#important_link').attr('class','now aa list-group-item scroll');
+}
+
+function now_schedule(){
+	$('.aa').each(function(){
+		$(this).attr('class','aa list-group-item scroll');
+	});
+	$('#schedule_link').attr('class','now aa list-group-item scroll');
+}
+
+function now_organization(){
+	$('.aa').each(function(){
+		$(this).attr('class','aa list-group-item scroll');
+	});
+	$('#organization_link').attr('class','now aa list-group-item scroll');
+}
+
+function now_reference(){
+	$('.aa').each(function(){
+		$(this).attr('class','aa list-group-item scroll');
+	});
+	$('#reference_link').attr('class','now aa list-group-item scroll');
+}
+
+function now_register(){
+	$('.aa').each(function(){
+		$(this).attr('class','aa list-group-item scroll');
+	});
+	$('#register_link').attr('class','now aa list-group-item scroll');
+}
+
+function now_hotel(){
+	$('.aa').each(function(){
+		$(this).attr('class','aa list-group-item scroll');
+	});
+	$('#hotel_link').attr('class','now aa list-group-item scroll');
+}
+
+function now_contact(){
+	$('.aa').each(function(){
+		$(this).attr('class','aa list-group-item scroll');
+	});
+	$('#contact_link').attr('class','now aa list-group-item scroll');
+}
+
